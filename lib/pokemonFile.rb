40e6d468b80b1b5c723dc4eec2ba8f9a5b799bb3
collinl34 <collinlshelby@gmail.com>
2017-04-28 14:01:20 -0700

    pokemon db populate with all names and pokemon types

--- lib/pokemonFile.rb
+++ lib/pokemonFile.rb
@@ -1,20 +1,21 @@
-# require "PokemonGenerator/version"
-require 'json'
 require 'nokogiri'
 require 'open-uri'
-# require 'awesome_print'
-require 'csv'
+require 'awesome_print'
+require 'json'
 
 def parse_add_file
   html = Nokogiri::HTML(open("https://pokemondb.net/pokedex/all"))
-  CSV.open('lib/pokemon.csv', 'wb') do |file|
-    html.css('.ent-name').each do |item|
-      file << [item.text]
+  File.open('./pokemon.json', 'wb') do |file|
+    html.css('tr').each do |txt|
+      pokemon = {
+
+        "name": txt.css('.ent-name').text,
+        "type": txt.css('.type-icon').text
+
+      }
+      file.write(pokemon.to_json)  
     end
   end
-end
-
-def name
 end
 
 parse_add_file()
