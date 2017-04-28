015703931735527c980861a5f0c1b0cb1d4bc581
collinl34 <collinlshelby@gmail.com>
2017-04-27 17:16:02 -0700

    json parser method add to module along with method to parse json file for skills and power and pp

--- spec/PokemonGenerator_spec.rb
+++ spec/PokemonGenerator_spec.rb
@@ -1,15 +1,17 @@
 require "spec_helper"
 
 RSpec.describe PokemonGenerator do
+
   it "has a version number" do
     expect(PokemonGenerator::VERSION).not_to be nil
   end
 
-  it 'returns a random named pokemon' do 
+  it 'returns a random named pokemon' do
     expect(PokemonGenerator.name).to be_a String
   end
 
-  it 'returns a skill for the specific pokemon type' do 
+  it 'returns a skill for the specific pokemon type' do
     expect(PokemonGenerator.skills).to eq 'something'
   end
+  
 end
